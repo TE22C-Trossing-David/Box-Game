658bb732d2f14f526e62529e7d5e07a4f766a873
David Trossing <David.Trossing@elev.ga.ntig.se>
2024-02-21 11:16:37 +0100

    LÅDOR!!

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,21 +1,47 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 
 Raylib.InitWindow(800, 600, "Slutprojekt");
-Raylib.SetTargetFPS(60);
+Raylib.SetTargetFPS(200);
+
+Rectangle[,] boxes = new Rectangle[3, 5];
+
 
 
 
 /*--<][Main Loop][>--------------------------------------------------------------------------------------------------------------------------------------------------------------- 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
+for (int x = 0; x < boxes.GetLength(0); x++)
+{
+  for (int y = 0; y < boxes.GetLength(1); y++)
+  {
+    Rectangle rect = new(x * 120 + 50, y * 90 + 30, 100, 50);
+    boxes[x, y] = rect;
+  }
 
+}
 
 while (!Raylib.WindowShouldClose())
 {
-  
+
+Vector2 mousePosVect = new Vector2(Raylib.GetMouseX(), Raylib.GetMouseY());
+
 
   Raylib.BeginDrawing();
   Raylib.ClearBackground(Color.White);
+
+  for (int x = 0; x < boxes.GetLength(0); x++)
+  {
+    for (int y = 0; y < boxes.GetLength(1); y++)
+    {
+      Raylib.DrawRectangleRec(boxes[x, y], Color.Brown);
+      if (Raylib.IsMouseButtonPressed(MouseButton.Left) && Raylib.CheckCollisionPointRec(mousePosVect, boxes[x, y]))
+      {
+        
+      }
+    }
+
+  } 
   Raylib.EndDrawing();
 }
-
