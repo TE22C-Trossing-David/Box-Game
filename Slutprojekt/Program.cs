3c39844a745ec6bd27a12f5b03b2c47321d26962
David Trossing <David.Trossing@elev.ga.ntig.se>
2024-03-06 11:16:34 +0100

    Bocese

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,32 +1,26 @@
-﻿using System.Numerics;
+﻿using System.Linq.Expressions;
+using System.Numerics;
 using Raylib_cs;
 
 Raylib.InitWindow(800, 600, "Slutprojekt");
 Raylib.SetTargetFPS(200);
 
-Rectangle[,] boxes = new Rectangle[3, 5];
-
-
+  Vector2 boxPressedV = new Vector2();
+    Rectangle[,] boxes = new Rectangle[3, 5];
 
 
 /*--<][Main Loop][>--------------------------------------------------------------------------------------------------------------------------------------------------------------- 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
-for (int x = 0; x < boxes.GetLength(0); x++)
-{
-  for (int y = 0; y < boxes.GetLength(1); y++)
-  {
-    Rectangle rect = new(x * 120 + 50, y * 90 + 30, 100, 50);
-    boxes[x, y] = rect;
-  }
-
-}
 
 while (!Raylib.WindowShouldClose())
 {
 
-Vector2 mousePosVect = new Vector2(Raylib.GetMouseX(), Raylib.GetMouseY());
+  Vector2 mousePosV = new Vector2(Raylib.GetMouseX(), Raylib.GetMouseY());
 
+
+
+  BoxCreation(5,3);
 
   Raylib.BeginDrawing();
   Raylib.ClearBackground(Color.White);
@@ -36,12 +30,42 @@ Vector2 mousePosVect = new Vector2(Raylib.GetMouseX(), Raylib.GetMouseY());
     for (int y = 0; y < boxes.GetLength(1); y++)
     {
       Raylib.DrawRectangleRec(boxes[x, y], Color.Brown);
-      if (Raylib.IsMouseButtonPressed(MouseButton.Left) && Raylib.CheckCollisionPointRec(mousePosVect, boxes[x, y]))
+    }
+  }
+
+
+  // Sparar vilken box du har klickat på
+  if (Raylib.IsMouseButtonPressed(MouseButton.Left))
+  {
+    for (int x = 0; x < boxes.GetLength(0); x++)
+    {
+      for (int y = 0; y < boxes.GetLength(1); y++)
       {
-        
+        if (Raylib.CheckCollisionPointRec(mousePosV, boxes[x, y]))
+        {
+          boxPressedV.X = x;
+          boxPressedV.Y = y;
+        }
       }
     }
+  }
 
-  } 
+
+
   Raylib.EndDrawing();
+}
+
+
+void BoxCreation(int a,int b)
+{
+  for (int x = 0; x < boxes.GetLength(0); x++)
+  {
+    for (int y = 0; y < boxes.GetLength(1); y++)
+    {
+      Rectangle rect = new(x * 120 + 50, y * 90 + 30, 100, 50);
+      boxes[x, y] = rect;
+    }
+
+  }
+
 }
